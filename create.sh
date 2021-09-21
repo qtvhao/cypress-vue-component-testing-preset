19c7d3e09032d5a8429133eec04da812e7f5eb36
Hào Nghiêm <qtvhao@gmail.com>
2021-09-21 12:03:08 +0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,5 @@
 yarn add cypress @cypress/vue @cypress/webpack-dev-server --dev
+curl https://raw.githubusercontent.com/qtvhao/cypress-vue-component-testing-preset/main/src/components/HelloWorld.spec.ts --create-dirs --output ./src/components/HelloWorld.spec.ts
 curl https://raw.githubusercontent.com/qtvhao/cypress-vue-component-testing-preset/main/cypress/plugins/index.js --create-dirs --output ./cypress/plugins/index.js
 curl https://raw.githubusercontent.com/qtvhao/cypress-vue-component-testing-preset/main/cypress.json --output ./cypress.json
+
